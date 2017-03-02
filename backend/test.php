5fb0123fc1db111a751eb5add2cd9cf7a5fa41df
mvillarreal <villarreal.m@osperyh.org>
2017-03-02 13:45:42 -0300

    array

--- backend/test.php
+++ backend/test.php
@@ -1,10 +1,26 @@
 <?php
+	class Thing
+	{
+		public $title;
+		public $description;
+		public $url;
+	}
+
+
     if (isset($_POST['name']) && isset($_POST['info']))
     {
         echo "<strong>Post received.</strong> <br/> <br/> <strong>Name:</strong> " . $_POST['name'] . "<br/><strong>Info:</strong> " . $_POST['info'];
     }
     else
     {
-        echo "Post not received.";
+		var t1 = new Thing();
+		t1.->$title = 'plancha';
+		var t2 = new Thing();
+		t2.->$title = 'savana';
+		$array = array(
+         t1,t2
+		 );
+	
+        echo json_encode($array);
     }
 ?>
